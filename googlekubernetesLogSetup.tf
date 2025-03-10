45a559d51bfa9566cf4b1e0217601861c57292f1
Alekhya0824 <168534320+Alekhya0824@users.noreply.github.com>
2025-03-10 15:25:29 +0530

    Update googlekubernetesLogSetup.tf

--- googlekubernetesLogSetup.tf
+++ googlekubernetesLogSetup.tf
@@ -50,7 +50,7 @@ resource "google_logging_project_sink" "sentinel-sink" {
   destination = "pubsub.googleapis.com/projects/${data.google_project.project.project_id}/topics/${var.topic-name}"
   depends_on = [google_pubsub_topic.sentinelgooglekubernetes-topic]
 
-  filter = "resource.type="k8s_cluster"
+  filter = "(resource.type="k8s_cluster")"
   unique_writer_identity = true
 }
 
@@ -60,7 +60,7 @@ resource "google_logging_organization_sink" "sentinel-organization-sink" {
   org_id = var.organization-id
   destination = "pubsub.googleapis.com/projects/${data.google_project.project.project_id}/topics/${var.topic-name}"
 
-  filter = "resource.type="k8s_cluster"
+  filter = "(resource.type="k8s_cluster")"
   include_children = true
 }
 
